5beeb0c4ec250be759f15c76a03900a3e3c3e731
fushang318 <fushang318@gmail.com>
2021-05-21 17:04:39 +0800

    add 3 person test youhua

--- logic/ei_issue_pair_issue_processor.py
+++ logic/ei_issue_pair_issue_processor.py
@@ -15,8 +15,13 @@ class EiIssuePairIssueProcessor:
 
         self.user_count, self.issue_count, self.issue_more_half_user_name = self.get_total_info()
 
+        # 成功配对中，ISSUE 出现的次数
         # {'issue_id': pair_count}
         self.pair_dict = {}
+        # 已经成功配对过的
+        self.ei_issue_pair_is_in = {}
+        # 已经尝试配对，但是不符合要求的配对
+        self.ei_issue_pair_id_blacklist_is_in = {}
 
     def pair_success(self):
         return self.pair_success
@@ -30,11 +35,16 @@ class EiIssuePairIssueProcessor:
             self.pair_success = True
             return
 
-        print("开始配对...")
         self.pair_success = False
-        self.pair_ei_issue_first()
-        if len(self.ei_issue_pair_list) != len(self.ei_issue_list):
-            self.pair_ei_issue_addi()
+        for i in range(20):
+            print("开始配对，第 {} 次...".format(i+1))
+            self.pair_ei_issue()
+            if len(self.ei_issue_pair_list) == len(self.ei_issue_list):
+                break
+            print("配对数量：{}/{}".format(
+                len(self.ei_issue_pair_list),
+                len(self.ei_issue_list)
+            ))
 
         if len(self.ei_issue_pair_list) == len(self.ei_issue_list):
             self.pair_success = True
@@ -63,24 +73,22 @@ class EiIssuePairIssueProcessor:
             "pair_success": self.pair_success
         }
 
-    def pair_ei_issue_first(self):
+    def pair_ei_issue(self):
         ei_issue_list_part_1 = self.ei_issue_list[:]
         ei_issue_list_part_2 = self.ei_issue_list[:]
 
-        weight_processor = EiIssueWeightProcessor(self.user_count, self.issue_count, self.issue_more_half_user_name, self.ei_config, [])
+        weight_processor = EiIssueWeightProcessor(self.user_count, self.issue_count, self.issue_more_half_user_name, self.ei_config, self.pair_dict, self.ei_issue_pair_id_blacklist_is_in)
         key_2_weight = {}
 
         # 获取权重矩阵
         weight_matrix = [[0 for i in range(len(ei_issue_list_part_2))] for i in range(len(ei_issue_list_part_1))]
         for x_index, x_ei_issue in enumerate(ei_issue_list_part_1):
             for y_index, y_ei_issue in enumerate(ei_issue_list_part_2):
-                # print("y_index {}".format(y_index))
                 weight = weight_processor.get_weight(x_ei_issue, y_ei_issue)
                 weight_matrix[x_index][y_index] = weight
                 key = "{}_{}".format(x_index, y_index)
                 key_2_weight[key] = weight
                 ep = EiIssuePair(x_ei_issue, y_ei_issue)
-                weight_processor.add_pair(ep)
 
         km = KuhnMunkres(weight_matrix)
         km.run()
@@ -102,31 +110,10 @@ class EiIssuePairIssueProcessor:
             right = i[2]
             if self.is_valid(left, right, weight):
                 self.add_ei_issue_pair(left, right, weight)
+            else:
+                ep = EiIssuePair(left, right)
+                self.ei_issue_pair_id_blacklist_is_in[ep.id] = True
 
-    def pair_ei_issue_addi(self):
-        ei_issue_list_part_1 = self.ei_issue_list[:]
-        ei_issue_list_part_2 = self.ei_issue_list[:]
-
-        weight_processor = EiIssueWeightProcessor(self.user_count, self.issue_count, self.issue_more_half_user_name, self.ei_config, self.ei_issue_pair_list)
-
-        # 获取权重矩阵
-        weight_list = []
-        weight_matrix = [[0 for i in range(len(ei_issue_list_part_2))] for i in range(len(ei_issue_list_part_1))]
-        for x_index, x_ei_issue in enumerate(ei_issue_list_part_1):
-            for y_index, y_ei_issue in enumerate(ei_issue_list_part_2):
-                weight = weight_processor.get_weight(x_ei_issue, y_ei_issue)
-                weight_matrix[x_index][y_index] = weight
-                ep = EiIssuePair(x_ei_issue, y_ei_issue)
-                weight_processor.add_pair(ep)
-                weight_list.append([weight, x_ei_issue, y_ei_issue])
-        weight_list = sorted(weight_list, key=lambda a: a[0], reverse=True)  # 进行排序，使得权值搞的优先进入pair
-        for item in weight_list:
-            weight = item[0]
-            left = item[1]
-            right = item[2]
-            if self.is_valid(left, right, weight):
-                self.add_ei_issue_pair(left, right, weight)
- 
     def is_valid(self, left, right, weight):
         if weight <= 0:
             return False
@@ -138,16 +125,30 @@ class EiIssuePairIssueProcessor:
         if self.pair_dict[right.id] >= 2:
             return False
 
+        # 配对第二次出现
+        #     一个ISSUE时，允许 flag_1
+        #     人数大于3时，允许 flag_2
+        #     其他情况，不允许
+        flag_1 = self.user_count == 1 and self.issue_count == 1
+        flag_2 = self.user_count > 3
+        if not flag_1 and not flag_2:
+            pair = EiIssuePair(left, right)
+            if self.ei_issue_pair_is_in.get(pair.id, False):
+                return False
+
         return True
     
     def add_ei_issue_pair(self, left, right, weight):
         ep = EiIssuePair(left, right)
+        self.ei_issue_pair_list.append(ep)
+        self.total_weight += weight
+
+        self.ei_issue_pair_is_in[ep.id] = True
+
         self.pair_dict.setdefault(left.id, 0)
         self.pair_dict.setdefault(right.id, 0)
         self.pair_dict[left.id] += 1
         self.pair_dict[right.id] += 1
-        self.ei_issue_pair_list.append(ep)
-        self.total_weight += weight
 
     def get_total_info(self):
         issue_more_half_user_name = None
--- logic/ei_issue_weight_processor.py
+++ logic/ei_issue_weight_processor.py
@@ -6,44 +6,23 @@ class EiIssueWeightProcessor:
     """
     issue 配对权重计算
     """
-    def __init__(self, user_count, issue_count, issue_more_half_user_name, ei_config, issue_pair_list):
+    def __init__(self, user_count, issue_count, issue_more_half_user_name, ei_config, prev_issue_count, ei_issue_pair_id_blacklist_is_in):
         self.issue_more_half_user_name = issue_more_half_user_name
         self.user_count = user_count
         self.issue_count = issue_count
         self.ei_config = ei_config
-        self.issue_pair_list = issue_pair_list
 
-        self.issue_pair_is_in = {}
+        self.prev_issue_count = prev_issue_count
 
-        self.prev_issue_pair_is_in = {}
-        self.prev_issue_count = {}
-        self.build_prev_pair()
-
-    def build_prev_pair(self):
-        for pair in self.issue_pair_list:
-            self.prev_issue_pair_is_in[pair.id] = True
-
-            self.prev_issue_count.setdefault(pair.left.id, 0)
-            self.prev_issue_count.setdefault(pair.right.id, 0)
-            self.prev_issue_count[pair.left.id] += 1
-            self.prev_issue_count[pair.right.id] += 1
-
-    def add_pair(self, pair):
-        self.issue_pair_is_in[pair.pair_hash()] = True
+        self.ei_issue_pair_id_blacklist_is_in = ei_issue_pair_id_blacklist_is_in
+        self.has_blacklist = len(list(self.ei_issue_pair_id_blacklist_is_in.keys())) > 0
 
     def get_weight(self, ei_issue_1, ei_issue_2):
-        step_weight = 0
-        # 如果某人 ISSUE 数量过半，需要分两个阶梯
-        #   某人的 两个 ISSUE 进行配对 权重要低一个阶梯
-        #   其他配对  权重要高一个阶梯
-        if self.issue_more_half_user_name:
-            i_1_c = ei_issue_1.contributer.name
-            i_2_c = ei_issue_2.contributer.name
-            h_c = self.issue_more_half_user_name
-            if i_1_c == i_2_c and i_2_c == h_c:
-                step_weight = 1000
-            else:
-                step_weight = 2000
+        # 如果该配对在上几轮配对中已经尝试，并且不符合要求
+        if self.has_blacklist:
+            pair = EiIssuePair(ei_issue_1, ei_issue_2)
+            if self.ei_issue_pair_id_blacklist_is_in.get(pair.id, False):
+                return -10000
 
         # 非第一轮匹配需要查看 ISSUE 出现次数
         count_1 = self.prev_issue_count.get(ei_issue_1.id, 0)
@@ -73,27 +52,23 @@ class EiIssueWeightProcessor:
             if i_1_c == i_2_c:
                 return -10000
 
-        # 配对第二次出现
-        #     一个ISSUE时，允许 flag_1
-        #     人数大于3时，允许 flag_2
-        #     其他情况，不允许
-        flag_1 = self.user_count == 1 and self.issue_count == 1
-        flag_2 = self.user_count > 3
-
-        if not flag_1 and not flag_2:
-            pair = EiIssuePair(ei_issue_1, ei_issue_2)
-            if self.issue_pair_is_in.get(pair.pair_hash(), False):
-                raise "配对错误"
-            if self.issue_pair_is_in.get(pair.other_pair_hash(), False):
-                return -10000
-            if self.prev_issue_pair_is_in.get(pair.id, False):
-                return -10000
+        weight = 0
+        # 如果某人 ISSUE 数量过半，需要分两个阶梯
+        #   某人的 两个 ISSUE 进行配对 权重要低一个阶梯
+        #   其他配对  权重要高一个阶梯
+        if self.issue_more_half_user_name:
+            i_1_c = ei_issue_1.contributer.name
+            i_2_c = ei_issue_2.contributer.name
+            h_c = self.issue_more_half_user_name
+            if i_1_c == i_2_c and i_2_c == h_c:
+                weight += 2500
+            else:
+                weight += 5000
 
         # lable
         ei_issue_1_lable_info = ei_issue_1.label_info
         ei_issue_2_lable_info = ei_issue_2.label_info
 
-        weight = 0
         lables = set(ei_issue_1_lable_info.keys()) & set(ei_issue_2_lable_info.keys())
         for label in lables:
             weight += ei_issue_1_lable_info[label] * self.ei_config.ei_p_label_weight * self.ei_config.ei_p_label_weight_ratio
@@ -112,4 +87,4 @@ class EiIssueWeightProcessor:
         # 在相同值的情况下增加一个随机波动
         weight = weight*10 + (randrange(1, 100, 1)/10000)
 
-        return weight + step_weight
+        return weight

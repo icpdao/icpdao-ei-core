94d778d041e13f45b93fbe7275453886185949cb
fushang318 <fushang318@gmail.com>
2021-05-21 09:29:43 +0800

    add one issue test

---
+++ test/test_period_one_issue.py
@@ -0,0 +1,44 @@
+import json
+
+import os
+import json
+
+from models.ei_issue import EiIssue
+from models.ei_user import EiUser
+
+from logic.ei_processor import EiProcessor
+
+test_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), './')
+
+def test_first():
+    issues = json.load(open(os.path.join(test_path, 'data/period_one_issue_issues.json')))
+
+    ei_issue_list = []
+    for issue in issues:
+        contributer = EiUser(
+            name=issue['contributer']['name'],
+            labels=issue['contributer']['labels'],
+        )
+        reviewer = EiUser(
+            name=issue['reviewer']['name'],
+            labels=issue['reviewer']['labels'],
+        )
+        ei_issue = EiIssue(
+            _type = issue['type'],
+            org = issue['org'],
+            repo = issue['repo'],
+            number = issue['number'],
+            title = issue['title'],
+            contributer = contributer,
+            labels = issue['labels'],
+            size = issue['size'],
+            reviewer = reviewer,
+            pr_org = issue['pr_org'],
+            pr_repo = issue['pr_repo']
+        )
+        ei_issue_list.append(ei_issue)
+
+    ep = EiProcessor('one_issue', ei_issue_list)
+    ep.process()
+    assert ep.pair_success() == True
+    # json.dump(ep.assignees_info, open(os.path.join(test_path, 'data/period_one_issue_assignees_info.json'), "w"), ensure_ascii=False, indent=True)
